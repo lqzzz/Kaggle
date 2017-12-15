c644d872478cc5e4c0007f91e289e1d9187b67d5
闪闪 亮 <616478774@qq.com>
2017-12-15 23:13:15 +0800

    数据处理

--- Bike Sharing Demand/file.py
+++ Bike Sharing Demand/file.py
@@ -7,25 +7,67 @@ def TrainData():
     tr = pd.read_csv('train.csv')
     t =  pd.read_csv('test.csv')
     data = [tr,t]
-    
-    for i in range(2):
-        date = data[i].datetime.apply(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
-        data[i]['month'] = date.apply(lambda x: x.month)
-        data[i]['day']= date.apply(lambda x: x.day)
-        data[i]['hour'] = date.apply(lambda x: x.hour)
-        data[i] = data[i].drop('datetime',axis = 1)
     for i in data:
-        print(i.info())
-    lable = data[0]['count']
-    data[0] = data[0].drop('count',axis = 1)
-    # print(data.describe())
-    # print(data.corr()['count'])
-    # print(data.season)
-    # print(data.head())
-    a = 0
-    for i in range(54):
-        yield data[0][a:a+200],lable[a:a+200]
-        a += 200
-    
+        date = i.datetime.apply(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+        i['hour'] = date.apply(lambda x: x.hour)
+        # i = i.drop('datetime',axis = 1)
+    t = data[0]
+    t['t'] = 0.4 * t['temp'] + 0.6 * t['atemp'] 
+    t['tc'] = abs(t['temp'] - t['atemp'])
+    t['c'] = 1
+    d = t.loc[:,['tc','c']].groupby('tc',as_index = False).sum()
+    print(d)
+    # d = t.loc[:,['t','count']].groupby('t',as_index = False).sum()
+    # d = t.loc[:,['temp','atemp','count']].groupby(['temp','atemp'],as_index = False).mean()
+    # print(d.sort_values(by = 'count',ascending = False).corr()['count'])
+    # print(d.sort_values(by = 'count',ascending = False))
+    # print(d.corr()['count'])
+    # print(d)
+
+    # t['hotseason'] = 1
+    # t['hotseason'][t['season'] == 0] = 0
+
+    # t['hothour'] = 0
+
+    # t['hothour'][t['hour'] == 0] = 1
+    # t['hothour'][t['hour'] == 9] = 1
+    # t['hothour'][t['hour'] == 10] = 1
+    # t['hothour'][t['hour'] == 20] = 1
+    # t['hothour'][t['hour'] == 22] = 1
+    # t['hothour'][t['hour'] == 23] = 1
+
+    # t['hothour'][t['hour'] == 7] = 2
+    # t['hothour'][t['hour'] == 11] = 2
+    # t['hothour'][t['hour'] == 13] = 2
+    # t['hothour'][t['hour'] == 14] = 2
+    # t['hothour'][t['hour'] == 15] = 2
+    # t['hothour'][t['hour'] == 21] = 2
+
+
+    # t['hothour'][t['hour'] == 8] = 3
+    # t['hothour'][t['hour'] == 12] = 3
+    # t['hothour'][t['hour'] == 16] = 3
+    # t['hothour'][t['hour'] == 17] = 3
+    # t['hothour'][t['hour'] == 18] = 3
+    # t['hothour'][t['hour'] == 19] = 3
+
+    # d = t.loc[:,['hothour','count']].groupby('hothour',as_index = False).max().sort_values(by = 'count',ascending = True)
+    # print(d)
+    # print(d.corr()['count'])
+
+    # d = t.loc[:,['hour','count']].groupby('hour',as_index = False).max().sort_values(by = 'count',ascending = True)
+    # print(t['hothour'])
+    # print(t.hothour)
+    # print(d,'\n',d.corr()['count'])
+    # print(d.groupby('hour',axis = 0).sum().sort_values(by = 'count',ascending = True))
+    # t = t.drop(['datetime','casual','registered'],axis = 1)
+    # lable = t['count']
+    # print(t['hour'])
+    # t['CH'] = pd.cut(t['hour'], 5)
+    # print(t['CH'])
+    # a = 
+    # for i in range(54):
+    #     yield data[0][a:a+200],lable[a:a+200]
+    #     a += 200
 TrainData()
 
--- House Prices/file.py
+++ House Prices/file.py
@@ -2,16 +2,39 @@
 import numpy as np
 import pandas as pd
 # import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+def EDA():
+    train = pd.read_csv('train.csv')
+    test = pd.read_csv('test.csv')
+    quantitative = [f for f in train.columns if train.dtypes[f] != 'object']
+    quantitative.remove('SalePrice')
+    quantitative.remove('Id')
+    qualitative = [f for f in train.columns if train.dtypes[f] == 'object']
 
-def TrainData():
-    data = pd.read_csv('train.csv')
-    # print(data.info())
-    # print(data.MSZoning)
-    # print(data.describe().iloc[0:5,0:5])
-    print(data.drop('Id',axis = 1).corr().SalePrice)
-    print(data.Street.iloc[:5])
-    # print(data.iloc[0:4,0:10])
-    # train,label = data[:,1:],data[:,0]
+    missing = train.isnull().sum()
+    print(missing)
+    print()
+    missing = missing[missing > 0]
+    print(missing)
+    print()
+    missing.sort_values(inplace=True)
+    print(missing)
+    print()
+    # missing.plot.bar()
+    # plt.show()
+
+    # print(corr)
+    # full_data = train,test
+    # corr = train.drop('Id',axis = 1).corr().SalePrice
+    # print(corr)
+    # d = corr[corr<0.3].to_dict()
+    # train = train.drop(corr[corr<0.3].to_dict().keys(),axis = 1)
+    # print(train.info())
+    
+    # print(train.Street.iloc[:5])
+    # print(train.iloc[0:4,0:10])
+    # train,label = train[:,1:],train[:,0]
     # train = np.multiply(train,1.0/255)
     # l = np.zeros((42000,10))
     # for i in range(42000):
@@ -20,4 +43,4 @@ def TrainData():
     # for i in range(420):
     #     yield train[a:a + 100],l[a:a + 100]
     #     a += 100
-TrainData()
+EDA()
