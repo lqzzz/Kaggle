b76daa1d56b02f050b67fa5dc8a36a9d307a4f7a
闪闪 亮 <616478774@qq.com>
2017-12-17 22:09:00 +0800

    eda

--- Bike Sharing Demand/file.py
+++ Bike Sharing Demand/file.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import datetime as dt
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 def TrainData():
     tr = pd.read_csv('train.csv')
     t =  pd.read_csv('test.csv')
@@ -10,19 +13,37 @@ def TrainData():
     for i in data:
         date = i.datetime.apply(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
         i['hour'] = date.apply(lambda x: x.hour)
+        i['month'] = date.apply(lambda x: x.hour)
+        i['day'] = date.apply(lambda x: x.hour)
+        i['hour'] = date.apply(lambda x: x.hour)
         # i = i.drop('datetime',axis = 1)
     t = data[0]
-    t['t'] = 0.4 * t['temp'] + 0.6 * t['atemp'] 
     t['tc'] = abs(t['temp'] - t['atemp'])
-    t['c'] = 1
-    d = t.loc[:,['tc','c']].groupby('tc',as_index = False).sum()
-    print(d)
-    # d = t.loc[:,['t','count']].groupby('t',as_index = False).sum()
-    # d = t.loc[:,['temp','atemp','count']].groupby(['temp','atemp'],as_index = False).mean()
-    # print(d.sort_values(by = 'count',ascending = False).corr()['count'])
+    t = t[t['tc'] < 7]
+    # d = t.loc[:,['humidity','count']].groupby('humidity',as_index = False).mean()
+    # print(d.sort_values(by = 'humidity',ascending = False))
+    # print(d.corr()['count'])
+    fig,(ax1,ax2,ax3,ax4) = plt.subplots(ncols=4)
+    fig.set_size_inches(12, 5)
+    sns.regplot(x="temp", y="count", data=t,ax=ax1)
+    sns.regplot(x="windspeed", y="count", data=t,ax=ax2)
+    sns.regplot(x="humidity", y="count", data=t,ax=ax3)
+    sns.regplot(x="hour", y="count", data=t,ax=ax3)
+    plt.show()
+    # t['ts'] = 0
+    # t['ts'][(t.temp >= 10) & (t.temp < 21)] =1
+    # t['ts'][(t.temp >= 21) & (t.temp < 35)] =2
+    # t['ts'][t.temp >= 35] = 3
+
+    # d = t.loc[:,['ts','count']].groupby('ts',as_index = False).mean()
     # print(d.sort_values(by = 'count',ascending = False))
     # print(d.corr()['count'])
-    # print(d)
+
+    # d = t.loc[:,['temp','count']].groupby(['temp'],as_index = False).mean()
+    
+    # print(d.sort_values(by = 'count',ascending = False))
+    # print(d.sort_values(by = 'temp',ascending = False))
+    # print(d.corr()['count'])
 
     # t['hotseason'] = 1
     # t['hotseason'][t['season'] == 0] = 0
@@ -51,11 +72,11 @@ def TrainData():
     # t['hothour'][t['hour'] == 18] = 3
     # t['hothour'][t['hour'] == 19] = 3
 
-    # d = t.loc[:,['hothour','count']].groupby('hothour',as_index = False).max().sort_values(by = 'count',ascending = True)
+    # d = t.loc[:,['hothour','count']].groupby('hothour',as_index = False).count().sort_values(by = 'count',ascending = True)
     # print(d)
     # print(d.corr()['count'])
 
-    # d = t.loc[:,['hour','count']].groupby('hour',as_index = False).max().sort_values(by = 'count',ascending = True)
+    # d = t.loc[:,['hour','count']].groupby('hour',as_index = False).count().sort_values(by = 'count',ascending = True)
     # print(t['hothour'])
     # print(t.hothour)
     # print(d,'\n',d.corr()['count'])
